c7596dce6407d7d621752c3fd02d9fbe20f680b5
BillStephens2022 <two4onebill@yahoo.com>
2024-01-24 15:24:43 -0500

    added functionality to store and load data using a txt file and added a menu to the UI

--- src/main/java/com/billstephens/todolist/ToDoListApplication.java
+++ src/main/java/com/billstephens/todolist/ToDoListApplication.java
@@ -1,5 +1,6 @@
 package com.billstephens.todolist;
 
+import com.billstephens.todolist.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,5 +20,23 @@ public class ToDoListApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            TodoData.getInstance().storeTodoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadTodoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/com/billstephens/todolist/ToDoListController.java
+++ src/main/java/com/billstephens/todolist/ToDoListController.java
@@ -1,5 +1,6 @@
 package com.billstephens.todolist;
 
+import com.billstephens.todolist.datamodel.TodoData;
 import com.billstephens.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -11,6 +12,7 @@ import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,26 +29,28 @@ public class ToDoListController {
     private Label deadlineLabel;
 
     public void initialize() {
-        TodoItem item1 = new TodoItem("Mail birthday card",
-                "Buy a 30th birthday card for John",
-                LocalDate.of(2024, Month.JANUARY, 24));
-        TodoItem item2 = new TodoItem("Doctor's Appointment",
-                "See Dr. Smith at 123 Main St. Bring paperwork.",
-                LocalDate.of(2024, Month.FEBRUARY, 23));
-        TodoItem item3 = new TodoItem("Finish design proposal for client",
-                "I promised Mike I'd email website mockups by Friday JANUARY 26.",
-                LocalDate.of(2024, Month.JANUARY, 26));
-        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on December 10 on the 5:00 train",
-                LocalDate.of(2023, Month.DECEMBER, 10));
-        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
-                LocalDate.of(2024, Month.JANUARY, 31));
-
-        todoItems = new ArrayList<TodoItem>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
+//        TodoItem item1 = new TodoItem("Mail birthday card",
+//                "Buy a 30th birthday card for John",
+//                LocalDate.of(2024, Month.JANUARY, 24));
+//        TodoItem item2 = new TodoItem("Doctor's Appointment",
+//                "See Dr. Smith at 123 Main St. Bring paperwork.",
+//                LocalDate.of(2024, Month.FEBRUARY, 23));
+//        TodoItem item3 = new TodoItem("Finish design proposal for client",
+//                "I promised Mike I'd email website mockups by Friday JANUARY 26.",
+//                LocalDate.of(2024, Month.JANUARY, 26));
+//        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on December 10 on the 5:00 train",
+//                LocalDate.of(2023, Month.DECEMBER, 10));
+//        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
+//                LocalDate.of(2024, Month.JANUARY, 31));
+//
+//        todoItems = new ArrayList<TodoItem>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -54,11 +58,13 @@ public class ToDoListController {
                 if(newValue != null) {
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
                 }
             }
         });
 
-        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
 
